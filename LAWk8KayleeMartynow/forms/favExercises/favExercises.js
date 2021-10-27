762baf9944d67cbfce7e7cd31d88bd3febbb4b15
Kaylee Martynow <kayleemartynow@creighton.edu>
2021-10-27 14:39:16 -0500

    exercise

--- LAWk8KayleeMartynow/forms/favExercises/favExercises.js
+++ LAWk8KayleeMartynow/forms/favExercises/favExercises.js
@@ -1,10 +1,29 @@
 let exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 let coreExercises = ["situps","plank"]
+let responses = []
+
+
 
 favExercises.onshow=function(){
-
+selExercises.clear()
+      for (let y = 0; y<exercises.length; y++) {
+            selExercises.addItem(exercises[y])
+      }
 }
 
+selExercises.onclick = function() {
+      let val=selExercises.text
+      responses.push(val)
+      console.log(responses)
+}
 
-
+btnSubmit.onclick=function(){
+ responses.sort();
+      if (responses[1] == coreExercises[0] && responses[0] == coreExercises[1]) {
+            lblExercises.value='You chose situps and planks - those are the two core exercises.'
+      } else {
+            lblExercises.value='You did not pick the two core exercises.'
+            }
+      responses=[]
+} 
 
